2e200a15ddbdab941992ad6700a5a893a73fde64
Rachel Chan <chan.rce@gmail.com>
2020-11-12 12:15:32 -0500

    adding to app.py, need to fix temp route

--- Instructions/app.py
+++ Instructions/app.py
@@ -63,7 +63,8 @@ def precipitation():
     results_prcp = session.query(Measurement.date, Measurement.prcp).\
         filter(Measurement.date>=year_ago).\
         order_by(Measurement.date.desc()).all()
-
+    session.close()
+    
   # Create a dictionary from the row data and append to a list of all_passengers
     precip_values = []
     for date, prcp in results_prcp:
@@ -73,6 +74,51 @@ def precipitation():
         precip_values.append(prcp_dict)
 
     return jsonify(precip_values)
+
+#Station route
+
+@app.route("/api/v1.0/stations")
+def stations():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    "Return a list of all stations"
+
+    # Query all passengers
+    station_query = session.query(Station.station, Station.name).all()
+
+    session.close()
+
+    # Convert list of tuples into normal list
+    all_stations = list(np.ravel(station_query))
+
+    return jsonify(all_stations)
+
+
+
+#Temperature route
+
+@app.route("/api/v1.0/tobs")
+def temperature():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+    
+    # Query most active station
+    most_active = session.query(Measurement.date, Measurement.tobs) .\
+                filter(Measurement.station == 'USC00519281').filter(Measurement.date>=year_ago)
+
+
+    session.close()
+
+  # Create a dictionary from the row data and append to a list of all_passengers
+    temperature_list = []
+    for date, tobs in most_active:
+        temperature_dict = {}
+        temperature_dict["date"] = date
+        temperature_dict["prcp"] = prcp
+        temperature_list.append(temperature_dict)
+
+    return jsonify(temperature_list)
 
 
 
